874dc61b07500d719d4ea7e490812a50d4cf4d8b
Kunal Paliwal <kunalpaliwal2003@gmail.com>
2025-04-25 21:02:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio-pixalated/",
   server: {
     host: "::",
     port: 8080,
