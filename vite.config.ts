1b80fccd4aec248403f0247e8ced2b9de8e531ef
kunalpaliwal13 <kunalpaliwal2003@gmail.com>
2025-04-25 21:22:11 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/portfolio-pixalated/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: "/portfolio-pixalated/",  // This ensures the app works when deployed to a subdirectory
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
